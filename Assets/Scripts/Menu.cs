1b7b1708dd9122c7a62776a6c778f17be2e861c6
k4uang7me <jameskauan699@gmail.com>
2024-08-12 03:37:27 -0300

    Cutscenes

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,6 +7,26 @@ public class Menu : MonoBehaviour
 {
     public void PlayGame()
     {
+        SceneManager.LoadScene("Cutscene");
+    }
+
+        public void Cutscene()
+    {
+        SceneManager.LoadScene("Cutscene1");
+    }
+
+            public void Cutscene1()
+    {
+        SceneManager.LoadScene("Cutscene2");
+    }
+
+            public void Cutscene2()
+    {
         SceneManager.LoadScene("iniciar");
+    }
+
+    public void iniciar()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
